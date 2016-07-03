7d4b672ae1492af1e996425bc027948771c22e8d
makohn <marek.kohn@gmx.net>
2016-07-03 17:31:42 +0200

    FlashCardDao(Impl) um eine Update Methode erweitert, um zwischen Speichern und Aktualisieren unterscheiden zu koennen, da die ID beim Speichern noch nicht bekannt ist, beim Aktualisieren aber schon.

--- core/src/de/htwsaar/flashcards/dao/FlashCardDaoImpl.java
+++ core/src/de/htwsaar/flashcards/dao/FlashCardDaoImpl.java
@@ -16,7 +16,7 @@ public class FlashCardDaoImpl implements FlashCardDao {
 
 	private String sqlBefehl = new String("");
 	private Connection con;
-	private Statement dbBefehl = null;
+	private Statement dbBefehl;
 	private ResultSet rsDatenmenge;
 	
     public FlashCardDaoImpl () throws ClassNotFoundException {
@@ -35,13 +35,15 @@ public class FlashCardDaoImpl implements FlashCardDao {
 	        try {
 	            dbBefehl.executeUpdate(sql);
 	        } catch (SQLException ex) {
-	            System.err.println("Fehler beim löschen des Datensatzes!");
+	            System.err.println("Fehler beim loeschen des Datensatzes!");
 	        }	    
 	}
 
 	@Override
 	public void save(FlashCard flashcard) {
-        String sql = String.format("INSERT or replace INTO Cards VALUES (%d, \"%s\", \"%s\", \"%s\" , %d, %d, \"%s\")", flashcard.getCardId(), flashcard.getCardName(), flashcard.getCardQuestion(), flashcard.getCardAnswer(), flashcard.getBoxCounter(), flashcard.getStackId(), flashcard.getCardPicture());
+        String sql = String.format("INSERT INTO Cards "
+        + "(Card_Name, Card_Question, Card_Answer, Card_Stack_Id, Card_Picture_Link) " +
+        "VALUES (\"%s\", \"%s\", \"%s\" , %d, \"%s\")", flashcard.getCardName(), flashcard.getCardQuestion(), flashcard.getCardAnswer(),  flashcard.getStackId(), flashcard.getCardPicture());
         System.out.println(sql);
         try {
             dbBefehl.executeUpdate(sql);
@@ -51,12 +53,50 @@ public class FlashCardDaoImpl implements FlashCardDao {
     }
 
 	@Override
+	public void update(FlashCard flashcard) {
+		String sql = String.format("INSERT or replace INTO Cards VALUES (%d, \"%s\", \"%s\", \"%s\" , %d, %d, \"%s\")", flashcard.getCardId(), flashcard.getCardName(), flashcard.getCardQuestion(), flashcard.getCardAnswer(), flashcard.getBoxCounter(), flashcard.getStackId(), flashcard.getCardPicture());
+        System.out.println(sql);
+        try {
+            dbBefehl.executeUpdate(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(FlashCardDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	}	
+	
+	@Override
 	public List<FlashCard> getFlashCards() {
 
 		List<FlashCard> liste = new ArrayList<FlashCard>();
 
 		try {
 			sqlBefehl = "SELECT * FROM Cards;";
+			rsDatenmenge = dbBefehl.executeQuery(sqlBefehl);
+			while (rsDatenmenge.next()) {
+
+				int cardId = rsDatenmenge.getInt("Card_Id");
+				String cardName = rsDatenmenge.getString("Card_Name");
+				String cardQuestion = rsDatenmenge.getString("Card_Question");
+				String cardAAnswer = rsDatenmenge.getString("Card_Answer");
+				int boxCounter = rsDatenmenge.getInt("Card_Box_Counter");
+				int stackId = rsDatenmenge.getInt("Card_Stack_ID");
+				//int owner = rsDatenmenge.getInt("Card_Owner");
+				String cardPicture = rsDatenmenge.getString("Card_Picture_Link");
+
+				liste.add(new FlashCard(cardId, cardName, cardQuestion, cardAAnswer ,boxCounter ,stackId, cardPicture));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return liste;
+	}
+	
+	@Override
+	public List<FlashCard> getFlashCards(int box) {
+
+		List<FlashCard> liste = new ArrayList<FlashCard>();
+
+		try {
+			sqlBefehl = "SELECT * FROM Cards WHERE boxCount = " + box + ";";
 			rsDatenmenge = dbBefehl.executeQuery(sqlBefehl);
 			while (rsDatenmenge.next()) {
 
@@ -100,5 +140,7 @@ public class FlashCardDaoImpl implements FlashCardDao {
             System.err.println("Fehler beim laden des Datensatzes!");
         }	
 		return flashcard;
-	}	
+	}
+
+	
 }
--- core/src/de/htwsaar/flashcards/dao/interfaces/FlashCardDao.java
+++ core/src/de/htwsaar/flashcards/dao/interfaces/FlashCardDao.java
@@ -7,6 +7,8 @@ import de.htwsaar.flashcards.model.FlashCard;
 public interface FlashCardDao {
 	public void delete(FlashCard flashcard);
 	public void save(FlashCard flashcard);
+	public void update(FlashCard flashcard);
 	public List<FlashCard> getFlashCards(); //TODO: Add the deck as a parameter
+	public List<FlashCard> getFlashCards(int box);
 	public FlashCard get(int id);
 }
