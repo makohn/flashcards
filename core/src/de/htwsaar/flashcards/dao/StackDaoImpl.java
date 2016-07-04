f61227a2b184755e9c43eab87c10ca885d507ea4
MartinFk <martinfeick@yahoo.de>
2016-07-04 12:44:25 +0200

    StackDaoImpl Änderung

--- core/src/de/htwsaar/flashcards/dao/StackDaoImpl.java
+++ core/src/de/htwsaar/flashcards/dao/StackDaoImpl.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import de.htwsaar.flashcards.dao.interfaces.StackDao;
+import de.htwsaar.flashcards.model.Stack;
 
 public class StackDaoImpl implements StackDao {
 	
@@ -22,5 +24,35 @@ public class StackDaoImpl implements StackDao {
             System.err.println("Fehler beim erstellen des Statements");
         }
     }
+
+	@Override
+	public void delete(Stack stack) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(Stack stack) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Stack stack) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Stack> getStacks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Stack get(int StackId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- core/src/de/htwsaar/flashcards/dao/interfaces/StackDao.java
+++ core/src/de/htwsaar/flashcards/dao/interfaces/StackDao.java
@@ -2,7 +2,15 @@ package de.htwsaar.flashcards.dao.interfaces;
 
 import java.util.List;
 
+import de.htwsaar.flashcards.model.Stack;
+
 public interface StackDao {
+	
+	public void delete(Stack stack);
+	public void save(Stack stack);
+	public void update(Stack stack);
+	public List<Stack> getStacks();
+	public Stack get(int StackId);
 	
 
 }
