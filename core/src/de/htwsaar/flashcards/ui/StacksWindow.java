be3b60691b36c1438aaab030538dc55c4b741d2b
norasommer <nora@norasommer.de>
2016-07-11 09:54:13 +0200

    StacksWindow überarbeitet, Listener eingebaut, Variablennamen sprechend gemacht, Combobox eingebaut

--- core/src/de/htwsaar/flashcards/ui/StacksWindow.java
+++ core/src/de/htwsaar/flashcards/ui/StacksWindow.java
@@ -1,107 +1,154 @@
 package de.htwsaar.flashcards.ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
- 
+
 // Damit Objekte der Klasse StackWindow
 // zum ActionListener werden kann, muss das Interface
 // ActionListener implementiert werden
-public class StacksWindow extends JFrame implements ActionListener
+public class StacksWindow extends JFrame //implements ActionListener 
 {
-    JButton button1;
-    JButton button2;
-    JButton button3;
-    JButton button4;
-    JButton button5;
-    JButton button6;
-    JButton button7;
-    JButton button8;
-    JLabel label;
-    JPanel panel;
- 
-    public StacksWindow(){
-        this.setTitle("stacks");//Name des Fensters
-        this.setSize(800, 400); // Groesse des Fensters
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Fenster schliesst bei Anklicken des Kreuz
-        panel = new JPanel();
- 
-        // Leeres JLabel-Objekt wird erzeugt
-        label = new JLabel();
- 
-        //Buttons werden erstellt
-        button1 = new JButton("stackname");
-        button2 = new JButton ("edit");
-        button3 = new JButton ("delete");
-        button4 = new JButton ("export");
-        button5 = new JButton ("study");
-        button6 = new JButton ("study stack");
-        button7 = new JButton ("create new stack");
-        button8 = new JButton ("import stack");
-        //Buttons werden dem Listener zugeordnet
-        button1.addActionListener(this);
-        button2.addActionListener(this);
-        button3.addActionListener(this);
-        button4.addActionListener(this);
-        button5.addActionListener(this);
-        button6.addActionListener(this);
-        button7.addActionListener(this);
-        button8.addActionListener(this);
-        //Buttons werden dem JPanel hinzugefügt
-        panel.add(button1);
-        panel.add(button2);
-        panel.add(button3);
-        panel.add(button4);
-        panel.add(button5);
-        panel.add(button6);
-        panel.add(button7);
-        panel.add(button8);
-        //JLabel wird dem Panel hinzugefügt
-        panel.add(label);
-        this.add(panel);
-    }
- 
-    public static void main(String[] args)
-    {
-        // Ein neues Objekt der Klasse StacksWindow wird erzeugt
-        // und sichtbar gemacht
-    	StacksWindow sw = new StacksWindow();
-        sw.setVisible(true);
-    }
- 
-    public void actionPerformed (ActionEvent ae){
-        // Die Quelle wird mit getSource() abgefragt und mit den
-        // Buttons abgeglichen. Wenn die Quelle des ActionEvents einer
-        // der Buttons ist, wird der Text des JLabels entsprechend geändert
-        if(ae.getSource() == this.button1){
-            label.setText(("Button 1 wurde betätigt"));
-        }
-        else if(ae.getSource() == this.button2){
-            label.setText("Button 2 wurde betätigt");
-        }
-        else if (ae.getSource() == this.button3){
-            label.setText(("Button 3 wurde betätigt"));
-        }
-        
-        else if (ae.getSource() == this.button4){
-            label.setText(("Button 4 wurde betätigt"));
-        }
-        
-        else if (ae.getSource() == this.button5){
-            label.setText(("Button 5 wurde betätigt"));
-        }
-        
-        else if (ae.getSource() == this.button6){
-            label.setText(("Button 6 wurde betätigt"));
-        }
-        
-        else if (ae.getSource() == this.button7){
-            label.setText(("Button 7 wurde betätigt"));
-        }
-        
-        else if (ae.getSource() == this.button8){
-            label.setText(("Button 8 wurde betätigt"));
-        }
-    }
-}
 
+	private static final long serialVersionUID = 3603735413969513435L;
+
+	// Buttons
+	private JButton bntEdit;
+	private JButton bntDelete;
+	private JButton bntExport;
+	private JButton bntStudy;
+	private JButton bntStudyStack;
+	private JButton bntCreateStack;
+	private JButton bntImportStack;
+
+	// Label
+	private JLabel lstSelectStack;
+
+	// Panel
+	private JPanel pnlNorth;
+	private JPanel pnlCenter;
+	private JPanel pnlSouth;
+
+	// ComboBox
+	private JComboBox<String> jcoStackName;
+
+	// Stringarray zum Testen der Combobox
+	String stackslist[] = { "stack1", "stack2", "stack3", "stack4", "stack5", "stack6", "stack7" };
+
+	// Konstruktor
+	public StacksWindow(){
+        setTitle("stacks");//Name des Fensters
+        setSize(500, 500); // Groesse des Fensters
+        setLayout(new BorderLayout()); 
+
+       
+        
+        //3 Panelobjekte werden erzeugt
+        pnlNorth = new JPanel();
+        pnlCenter = new JPanel();
+        pnlSouth = new JPanel();
+        
+        // die Panels werden in das BorderLayout integriert
+        add(pnlNorth, BorderLayout.NORTH);		
+      	add(pnlCenter, BorderLayout.CENTER);
+      	add(pnlSouth, BorderLayout.SOUTH);
+      	
+      	
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Fenster schliesst bei Anklicken des Kreuz
+        setVisible(true);
+     
+      	
+      
+        //Komponenten werden erstellt
+      	jcoStackName = new JComboBox<String>(stackslist);
+        bntEdit = new JButton ("edit");
+        bntDelete = new JButton ("delete");
+        bntExport = new JButton ("export");
+        bntStudy = new JButton ("study");
+        bntStudyStack = new JButton ("study stack");
+        bntCreateStack = new JButton ("create stack");
+        bntImportStack = new JButton ("import stack");
+        lstSelectStack = new JLabel("Select Stack:");
+        
+
+        
+        //Buttons werden dem JPanel hinzugefÃ¼gt
+        pnlNorth.add(lstSelectStack);
+        pnlNorth.add(jcoStackName);
+        pnlNorth.add(bntEdit);
+        pnlNorth.add(bntDelete);
+        pnlNorth.add(bntExport);
+        pnlNorth.add(bntStudy);
+        pnlNorth.add(bntStudyStack);
+        pnlCenter.add(bntCreateStack);
+        pnlSouth.add(bntImportStack);
+      
+   
+		// Listener
+        jcoStackName.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+        
+		bntEdit.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+
+		bntDelete.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+        
+		bntExport.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+		
+		bntStudy.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+		
+		bntStudyStack.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+		
+		bntCreateStack.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+		
+		bntImportStack.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			}
+		});
+		
+		
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Fenster schliesst bei Anklicken des Kreuz
+        setVisible(true);
+	}
+	public static void main(String[] args) {
+		new StacksWindow();
+	}
+}
