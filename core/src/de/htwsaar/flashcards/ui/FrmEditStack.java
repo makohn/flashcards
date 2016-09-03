2bd09dbf06297c228dfb307290f917f81a48b4a9
makohn <marek.kohn@gmx.net>
2016-09-03 13:33:58 +0200

    Layout angepasst

--- core/src/de/htwsaar/flashcards/ui/FrmEditStack.java
+++ core/src/de/htwsaar/flashcards/ui/FrmEditStack.java
@@ -3,10 +3,12 @@ package de.htwsaar.flashcards.ui;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -17,12 +19,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
-import de.htwsaar.flashcards.engine.interfaces.GameEngine;
+import de.htwsaar.flashcards.service.EditFlashCardService;
 import de.htwsaar.flashcards.ui.component.GradientPanel;
 import de.htwsaar.flashcards.util.ButtonFactory;
+import de.htwsaar.flashcards.util.FlashCardUtils;
 
 public class FrmEditStack {
 
@@ -33,6 +36,7 @@ public class FrmEditStack {
 	private static final ImageIcon ICN_ARROW_RIGHT = new ImageIcon("res/images/arrow-right.png");
 	private static final ImageIcon ICN_ARROW_LEFT = new ImageIcon("res/images/arrow-left.png");
 	private static final ImageIcon ICN_ADD_PICTURE= new ImageIcon("res/images/add-picture.png");
+	private static final ImageIcon ICN_ADD_CARD= new ImageIcon("res/images/add-card.png");
 	
 	private static final Border INNER_CARD_BORDER = BorderFactory.createLineBorder(Color.GRAY);
 	private static final Border OUTER_CARD_BORDER = BorderFactory.createEmptyBorder(10,10,10,10);
@@ -41,7 +45,7 @@ public class FrmEditStack {
 	//private JLabel lblStackName;
 	private JLabel lblQuestion;
 	private JLabel lblAnswer;
-	private JLabel lblCardName;
+	private JTextField txtCardName;
 
 	// Input
 	private JTextArea txtQuestion;
@@ -54,6 +58,7 @@ public class FrmEditStack {
 	private JButton btnCancel;
 	private JButton btnDeleteCurrentCard;
 	private JButton btnSaveStack;
+	private JButton btnAddCard;
 
 	// Panels
 	private JPanel pnlNavigation;
@@ -61,12 +66,12 @@ public class FrmEditStack {
 	private JPanel pnlQuestion;
 	private JPanel pnlAnswer;
 	
-	private GameEngine engine;
+	private EditFlashCardService cardService;
 	
 	private JFrame editStackWindow;
 	
-	public FrmEditStack(GameEngine engine) {
-		this.engine = engine;
+	public FrmEditStack(EditFlashCardService cardService) {
+		this.cardService = cardService;
 		editStackWindow = new JFrame();
 		
 		initNavigationArea();
@@ -78,26 +83,31 @@ public class FrmEditStack {
 	}
 	
 	private void initNavigationArea() {
-		lblCardName = new JLabel(engine.getCurrentCard().getCardName());
-		btnCardForward = new JButton(ICN_ARROW_RIGHT);
-		btnCardForward.setContentAreaFilled(false);
-		btnCardForward.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-		btnCardBackward = new JButton(ICN_ARROW_LEFT);
-		btnCardBackward.setContentAreaFilled(false);
-		btnCardBackward.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-		
-		pnlNavigation = new JPanel();
+		txtCardName = new JTextField(cardService.getCurrentCard().getCardName());
+		txtCardName.setBorder(BorderFactory.createEmptyBorder());
+		txtCardName.setOpaque(false);
+		txtCardName.setPreferredSize(new Dimension(250,60));
+		txtCardName.setHorizontalAlignment(JTextField.CENTER);
+		btnCardForward = ButtonFactory.createImageButton(ICN_ARROW_RIGHT);
+		btnCardBackward = ButtonFactory.createImageButton(ICN_ARROW_LEFT);
+		btnAddCard = ButtonFactory.createImageButton(ICN_ADD_CARD);
+		btnAddCard.setToolTipText("Karte hinzufuegen");
+		pnlNavigation = new JPanel(new GridBagLayout());
 		pnlNavigation.setOpaque(false);
-		pnlNavigation.setLayout(new GridBagLayout());
 		GridBagConstraints gc = new GridBagConstraints();
-		gc.gridx = 0;
-		pnlNavigation.add(btnCardBackward, gc);
-		gc.gridx = 1;
-		gc.insets = new Insets(0, 40, 0, 40);
-		pnlNavigation.add(lblCardName, gc);
-		gc.insets = new Insets(0, 0, 0, 0);
-		gc.gridx = 2;
-		pnlNavigation.add(btnCardForward, gc);	
+		gc.gridx = gc.gridy = 0;
+		gc.insets = new Insets(0, 103, 0, 0);
+		pnlNavigation.add(btnCardBackward,gc);
+		gc.gridx++;
+		gc.insets = new Insets(0, 10, 0, 0);
+		pnlNavigation.add(txtCardName,gc);
+		gc.gridx++;
+		gc.insets = new Insets(0, 10, 0, 0);
+		pnlNavigation.add(btnCardForward,gc);
+		gc.gridx++;
+		gc.insets = new Insets(0, 40, 0, 0);
+		pnlNavigation.add(btnAddCard,gc);
+		pnlNavigation.setMaximumSize(new Dimension(650,200));
 	}
 	
 	private void initQuestionArea() {
@@ -108,17 +118,14 @@ public class FrmEditStack {
 		
 		lblQuestion = new JLabel(LABELSTRING_QUESTION);
 		lblQuestion.setAlignmentX(Component.CENTER_ALIGNMENT);
-		txtQuestion = new JTextArea(10,20);
-		txtQuestion.setLineWrap(true);
-		txtQuestion.setText(engine.getCurrentCard().getCardQuestion());
+		txtQuestion = FlashCardUtils.createCardTextArea(true);
+		txtQuestion.setText(cardService.getCurrentCard().getCardQuestion());
 		
-		btnAddPicture = new JButton(ICN_ADD_PICTURE);
-		btnAddPicture.setContentAreaFilled(false);
-		btnAddPicture.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		btnAddPicture = ButtonFactory.createImageButton(ICN_ADD_PICTURE);
 		btnAddPicture.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
 		JScrollPane scrlQuestion = new JScrollPane(txtQuestion);
-		scrlQuestion.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrlQuestion.setOpaque(false);
 		scrlQuestion.setBorder(INNER_CARD_BORDER);
 		
 		pnlQuestion.add(lblQuestion);
@@ -134,62 +141,80 @@ public class FrmEditStack {
 		
 		lblAnswer = new JLabel(LABELSTRING_ANSWER);
 		lblAnswer.setAlignmentX(Component.CENTER_ALIGNMENT);
-		txtAnswer = new JTextArea(10,20);
-		txtAnswer.setLineWrap(true);
-		txtAnswer.setText(engine.getCurrentCard().getCardAnswer());
+		txtAnswer = FlashCardUtils.createCardTextArea(true);
+		txtAnswer.setText(cardService.getCurrentCard().getCardAnswer());
 		
 		JScrollPane scrlAnswer = new JScrollPane(txtAnswer);
-		scrlAnswer.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrlAnswer.setBorder(BorderFactory.createCompoundBorder(OUTER_CARD_BORDER, INNER_CARD_BORDER));
-		scrlAnswer.setBorder(INNER_CARD_BORDER);
+        scrlAnswer.setOpaque(false);
+        scrlAnswer.setBorder(INNER_CARD_BORDER);
 		
 		pnlAnswer.add(lblAnswer);
 		pnlAnswer.add(scrlAnswer);
 	}
 	
 	private void initSaveDeleteArea() {
-		pnlSaveDelete = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		pnlSaveDelete = new JPanel(new GridLayout(1, 3, 10, 30));
 		pnlSaveDelete.setOpaque(false);
 		
 		btnCancel = ButtonFactory.createColouredButton("Abbrechen", ButtonFactory.BTN_BLUE);
 		btnDeleteCurrentCard = ButtonFactory.createColouredButton("Löschen", ButtonFactory.BTN_RED);
 		btnSaveStack = ButtonFactory.createColouredButton("Speichern", ButtonFactory.BTN_GREEN);
 		
-		pnlSaveDelete.add(btnCancel);
+		pnlSaveDelete.add(btnSaveStack);
 		pnlSaveDelete.add(btnDeleteCurrentCard);
-		pnlSaveDelete.add(btnSaveStack);	
+		pnlSaveDelete.add(btnCancel);
+		
+		pnlSaveDelete.setBorder(BorderFactory.createEmptyBorder(40, 0, 40, 0));
+		pnlSaveDelete.setMaximumSize(new Dimension(400,200));
 	}
 	
 	private void initFrame() {
 		JPanel mainPanel = new GradientPanel();
-		mainPanel.setLayout(new GridBagLayout());
-		
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.anchor = GridBagConstraints.FIRST_LINE_START;
-		//-------------------------------------
-		c.gridy=0;
-		mainPanel.add(pnlNavigation,c);
-		//-------------------------------------
-		c.gridwidth=3; 
-		c.gridy++;
-		mainPanel.add(pnlQuestion, c);
-		//-------------------------------------
-		c.gridy++;
-		mainPanel.add(pnlAnswer, c);
-		c.gridwidth=1;
-		//-------------------------------------
-		c.gridy++;
-		mainPanel.add(pnlSaveDelete,c);
-		
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+		mainPanel.add(pnlNavigation);
+		mainPanel.add(pnlQuestion);
+		mainPanel.add(pnlAnswer);
+		mainPanel.add(pnlSaveDelete);
 		editStackWindow.add(mainPanel);
 		editStackWindow.setVisible(true);
-		editStackWindow.setMinimumSize(new Dimension(400, 620));
+		editStackWindow.setMinimumSize(new Dimension(650, 780));
+		editStackWindow.setResizable(false);
 		editStackWindow.setTitle(FRAME_TITLE);
 		editStackWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	private void initListeners() {
+		btnCardForward.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cardService.nextCard();
+				updateTextFields();
+			}
+		});
 		
+		btnCardBackward.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cardService.previousCard();
+				updateTextFields();
+			}
+		});
+		
+		btnAddCard.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cardService.addCard();
+				txtCardName.setText("");
+				txtQuestion.setText("");
+				txtAnswer.setText("");
+			}
+		});
+		
+	}
+	
+	private void updateTextFields() {
+		txtCardName.setText(cardService.getCurrentCard().getCardName());
+		txtQuestion.setText(cardService.getCurrentCard().getCardQuestion());
+		txtAnswer.setText(cardService.getCurrentCard().getCardAnswer());
 	}
 }
